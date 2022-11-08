3c2d174d82f4cc764e8ce8684609e169fd8f77d5
Robson Barbosa <robson212.212@gmail.com>
2022-11-08 15:53:33 -0300

    handle clicked

--- src/routes/components/User.jsx
+++ src/routes/components/User.jsx
@@ -2,13 +2,17 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class User extends Component {
+  handleSelectUser = (e) => {
+    e.preventDefault();
+    console.log("Clicked");
+  };
   render() {
     const { user } = this.props;
-    console.log("teste:", user);
+
     return (
       <>
         <div className="flex flex-col items-center">
-          <div className="flex flex-row">
+          <div className="flex flex-row" onClick={this.handleSelectUser}>
             <img src={user.avatarURL} alt={user.name} className="w-14 h-14" />
             <span className="font-semibold px-4 self-center cursor-pointer hover:text-indigo-700 text-black">
               {user.name}
