38f2efbba8140c8eb0b106fb06a3944e69943bad
Robson Barbosa <robson212.212@gmail.com>
2022-11-10 17:27:28 -0300

    List of voted or not

--- src/routes/Question.jsx
+++ src/routes/Question.jsx
@@ -1,9 +1,18 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { connect } from "react-redux";
 import UnitQuestion from "./components/UnitQuestion";
 import { redirect, Link } from "react-router-dom";
 
 class Question extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      switchList: false,
+      //true for questionListAnswered
+      //false for questionListNotAnswered
+    };
+  }
+
   voted = (quest) => {
     const { authedUser } = this.props;
     const { optionOne, optionTwo } = quest;
@@ -16,32 +25,57 @@ class Question extends Component {
 
   render() {
     const questionList = this.props.questionsList;
+    const questionListNotAnswered = [];
+    const questionListAnswered = [];
 
-    console.log(this.props);
+    questionList.map((question) =>
+      this.voted(question)
+        ? questionListAnswered.push(question)
+        : questionListNotAnswered.push(question)
+    );
+
     return (
       <div>
-        <ul>
-          {questionList.map((question) => (
-            <Link to={`/questions/${question.id}`}>
-              {this.voted(question) ? (
-                <span>Votou</span>
-              ) : (
-                <span>Não votou</span>
-              )}
+        <button
+          onClick={() => this.setState({ switchList: false })}
+          className="mx-2 my-2 bg-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 rounded text-white px-8 py-3 text-sm"
+        >
+          Not voted
+        </button>
+        <button
+          onClick={() => this.setState({ switchList: true })}
+          className="mx-2 my-2 bg-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 rounded text-white px-8 py-3 text-sm"
+        >
+          Voted
+        </button>
 
-              <li key={question.id} className="my-10">
-                <UnitQuestion quest={question} />
-              </li>
-            </Link>
-          ))}
-        </ul>
+        {this.state.switchList ? (
+          <ul>
+            {questionListAnswered.map((question) => (
+              <Link to={`/questions/${question.id}`}>
+                <li key={question.id} className="my-10">
+                  <UnitQuestion quest={question} />
+                </li>
+              </Link>
+            ))}
+          </ul>
+        ) : (
+          <ul>
+            {questionListNotAnswered.map((question) => (
+              <Link to={`/questions/${question.id}`}>
+                <li key={question.id} className="my-10">
+                  <UnitQuestion quest={question} />
+                </li>
+              </Link>
+            ))}
+          </ul>
+        )}
       </div>
     );
   }
 }
 
 function mapStateToProps({ questions, authedUser, users }) {
-  console.log(questions);
   const questionArray = Object.values(questions);
   return {
     questionsList: questionArray,
